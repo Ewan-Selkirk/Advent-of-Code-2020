a100ec8a2ee0e4199a5af238a3b2e4318a2d0a71
Ewan-Selkirk <ewanselkirk@sky.com>
2020-12-09 03:53:01 +0000

    Completed Day 8 part 2 Updated README.md to show Day 8 part 2 complete

--- src/day8/day8.py
+++ src/day8/day8.py
@@ -8,14 +8,15 @@ def setup():
         instructions.append(line.rstrip().split())
 
 
-def day8_p1():
+def day8_p1(debug=True):
     accumulator = 0
     count = 0
     visited = []
 
     while True:
         if count in visited:
-            print("Loop detected!\nAccumulator =", accumulator)
+            if debug:
+                print("Loop detected!")
             break
         else:
             visited.append(count)
@@ -23,19 +24,39 @@ def day8_p1():
         if instructions[count][0] == "acc":
             accumulator += int(instructions[count][1])
             count += 1
-            continue
         elif instructions[count][0] == "jmp":
             count += int(instructions[count][1])
-            continue
         else:
             count += 1
 
+        if count == len(instructions):
+            return True, accumulator
+
+    return False, accumulator
+
 
 def day8_p2():
-    pass
+    for i in range(len(instructions)):
+        if instructions[i][0] == "nop":
+            instructions[i][0] = "jmp"
+
+            run = day8_p1(debug=False)
+            if run[0]:
+                return run[1]
+            else:
+                instructions[i][0] = "nop"
+
+        elif instructions[i][0] == "jmp":
+            instructions[i][0] = "nop"
+
+            run = day8_p1(debug=False)
+            if run[0]:
+                return run[1]
+            else:
+                instructions[i][0] = "jmp"
 
 
 if __name__ == '__main__':
     setup()
-    day8_p1()
-    day8_p2()
+    print(day8_p1(debug=False)[1])
+    print(day8_p2())
